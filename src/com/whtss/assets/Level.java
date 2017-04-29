fcb155414347f1bde4b559533a446d04ecac1945
Cameron Jenkins <cameronjenkins@192.168.1.229>
2017-04-29 14:32:47 -0400

    added a random

--- src/com/whtss/assets/Level.java
+++ src/com/whtss/assets/Level.java
@@ -15,7 +15,10 @@ public class Level
 		floorLayer = new Integer[42][32];
 		HexPoint.iterateArray(floorLayer, (HexPoint hex, int x, int y) ->
 		{
-			floorLayer[x][y] = (int)(Math.random() * 200) | (int)(Math.random() * 200) << 8 | (int)(Math.random() * 200) << 16;
+			if(Math.random()<.25)
+			 floorLayer[x][y] = (int)(Math.random() * 200) | (int)(Math.random() * 200) << 8 | (int)(Math.random() * 200) << 16;
+			else
+		     floorLayer[x][y] = (int)(1) | (int)(1) << 8 | (int)(1) << 16;
 		});
 	}
 
