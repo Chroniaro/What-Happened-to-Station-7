f1ca1a84ae73b276aed0b490bf90fdfef2a10b52
Cameron Jenkins <cameronjenkins@10.122.187.63>
2017-05-01 12:04:19 -0400

    added a random start point genorator

--- src/com/whtss/assets/Level.java
+++ src/com/whtss/assets/Level.java
@@ -1,15 +1,17 @@
 package com.whtss.assets;
 
 import java.util.List;
+import java.util.Random;
 
 public class Level
 {
+	Random rand = new Random();
 	//Layers
 	Integer[][] floorLayer;
 	LevelObject[][] objectLayer;
 	List<Entity> entities;
-	int x = 1;
-	
+	int c = rand.nextInt(41 - 0 + 1) + 0;
+	int v = rand.nextInt(19 - 0 + 1) + 0;
 	public Level()
 	{
 		floorLayer = new Integer[41][18];
@@ -22,6 +24,14 @@ public class Level
 	public int getWidth()
 	{
 		return floorLayer.length;
+	}
+	public int getEntrance()
+	{
+		return floorLayer.length;
+	}
+	public int getstart()
+	{
+		return floorLayer[c][v];
 	}
 
 	public int getHeight()
