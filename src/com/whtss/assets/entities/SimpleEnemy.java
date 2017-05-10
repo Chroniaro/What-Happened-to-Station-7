02a5dc93a8d686e10a2021832a0e4a168d05eab6
Cameron Jenkins <cameronjenkins@10.122.183.204>
2017-05-10 10:19:36 -0400

    # WARNING: head commit changed in the meantime
    sd

--- src/com/whtss/assets/entities/SimpleEnemy.java
+++ src/com/whtss/assets/entities/SimpleEnemy.java
@@ -6,8 +6,7 @@ import com.whtss.assets.core.Entity;
 import com.whtss.assets.core.Level;
 import com.whtss.assets.hex.HexPoint;
 
-public class SimpleEnemy extends Entity implements Damageable
-{
+public class SimpleEnemy extends Entity implements Damageable {
 	Random rand = new Random();
 	int speed = 5;
 	int move = 0;
@@ -15,142 +14,123 @@ public class SimpleEnemy extends Entity implements Damageable
 	int health = 100;
 	int playersleft = 0;
 
-	public void enmove()
-	{
+	public void enmove() {
 		move(0, 2, 2);
 	}
 
 	// @Override
 	@UIEventHandle(value = "Next Turn", turn = "Enemy")
-	public void Turn()
-	{
+	public void Turn() {
 
-//		System.out.println("Turn");
+		// System.out.println("Turn");
 
 		// takeDamage(10);
-		for (int i = 0; i <= 10; i++)
-		{
+		for (int i = 0; i <= 10; i++) {
 			goveryclose();
-			Player best = null;
-			for (Entity e : getLevel().getEntities())
-				if (getLocation().dist(e.getLocation()) < 6)
-					if (e instanceof Player)
-						best = (Player) e;
+			for (int ii = 0; ii <= 5; ii++) {
+				Player best = null;
+				for (Entity e : getLevel().getEntities())
+					if (getLocation().dist(e.getLocation()) < 6 && e.isActive() == true) {
+						if (e instanceof Player) {
+							best = (Player) e;
+							best.takeDamage(7);
+							System.out.println(best + " " + getLocation().dist(best.getLocation()));
+						}
+					}
+			}
 
-			System.out.println(best + " " + getLocation().dist(best.getLocation()));
-			best.takeDamage(7);
 		}
 	}
 
-	public void goveryclose()
-	{
-		Entity best = null;
-		int min = 12345678;
-		for (Entity e : getLevel().getEntities())
-		{
+	public void goveryclose() {
+		Entity bestest = null;
+		int min = 11111111;
+		for (Entity e : getLevel().getEntities()) {
 			e.getLocation();
 
-			if (min > getLocation().dist(e.getLocation()) && getLocation() != e.getLocation())
-				best = e;
+			if (min > getLocation().dist(e.getLocation()) && getLocation() != e.getLocation() && e.isActive() == true)
+				bestest = e;
 		}
 		int a = 0;
 		int b = 0;
 		int y = 0;
-		if (best.getLocation().getA() <= getLocation().getA())
-		{
-			a = best.getLocation().getA() - getLocation().getA();
+		if (bestest.getLocation().getA() <= getLocation().getA()) {
+			a = bestest.getLocation().getA() - getLocation().getA();
 		}
-		if (best.getLocation().getB() <= getLocation().getB())
-		{
-			b = best.getLocation().getB() - getLocation().getB();
+		if (bestest.getLocation().getB() <= getLocation().getB()) {
+			b = bestest.getLocation().getB() - getLocation().getB();
 		}
-		if (best.getLocation().getY() <= getLocation().getY())
-		{
-			y = best.getLocation().getY() - getLocation().getY();
+		if (bestest.getLocation().getY() <= getLocation().getY()) {
+			y = bestest.getLocation().getY() - getLocation().getY();
 		}
-		if (best.getLocation().getA() < getLocation().getA())
-		{
-			a = getLocation().getA() - best.getLocation().getA();
+		if (bestest.getLocation().getA() < getLocation().getA()) {
+			a = getLocation().getA() - bestest.getLocation().getA();
 		}
-		if (best.getLocation().getB() < getLocation().getB())
-		{
-			b = getLocation().getB() - best.getLocation().getB();
+		if (bestest.getLocation().getB() < getLocation().getB()) {
+			b = getLocation().getB() - bestest.getLocation().getB();
 		}
-		if (best.getLocation().getY() < getLocation().getY())
-		{
-			y = getLocation().getY() - best.getLocation().getY();
+		if (bestest.getLocation().getY() < getLocation().getY()) {
+			y = getLocation().getY() - bestest.getLocation().getY();
 		}
 		int u = 0;
 		int uu = 0;
 		int uuu = 0;
-		if (y >= 1)
-		{
+		if (y >= 1) {
 			uuu = 1;
 		}
-		if (a >= 1)
-		{
+		if (a >= 1) {
 			u = -1;
 		}
-		if (b >= 1)
-		{
+		if (b >= 1) {
 			uu = -1;
 		}
-		if (y < 1)
-		{
+		if (y < 1) {
 			uuu = -1;
 		}
-		if (a < 1)
-		{
+		if (a < 1) {
 			u = 1;
 		}
-		if (b < 1)
-		{
+		if (b < 1) {
 			uu = 1;
 		}
 
 		move(u, uu, uuu);
 
-		//		System.out.print("best");
-		//		System.out.print(" " + best.getLocation().getA());
-		//		System.out.print(" " + best.getLocation().getB());
-		//		System.out.print(" " + best.getLocation().getY());
-		//		System.out.println(" ");
-		//		System.out.print("current");
-		//		System.out.print(" " + getLocation().getA());
-		//		System.out.print(" " + getLocation().getB());
-		//		System.out.print(" " + getLocation().getY());
-		//		System.out.println(" ");
-		//		System.out.print(a);
-		//		System.out.print("," + b);
-		//		System.out.print("," + y);
-		//		System.out.println(" ");
+		// System.out.print("best");
+		// System.out.print(" " + best.getLocation().getA());
+		// System.out.print(" " + best.getLocation().getB());
+		// System.out.print(" " + best.getLocation().getY());
+		// System.out.println(" ");
+		// System.out.print("current");
+		// System.out.print(" " + getLocation().getA());
+		// System.out.print(" " + getLocation().getB());
+		// System.out.print(" " + getLocation().getY());
+		// System.out.println(" ");
+		// System.out.print(a);
+		// System.out.print("," + b);
+		// System.out.print("," + y);
+		// System.out.println(" ");
 	}
 
-	public int gethealth()
-	{
+	public int gethealth() {
 		return health;
 	}
 
-	public int getdist(int da, int db, int dhy)
-	{
+	public int getdist(int da, int db, int dhy) {
 		int dist = Math.abs(da) + Math.abs(db) + Math.abs(dhy);
 		return dist;
 	}
 
-	public SimpleEnemy(HexPoint location, Level level)
-	{
+	public SimpleEnemy(HexPoint location, Level level) {
 		super(location, level);
 	}
 
-	public void noone()
-	{
+	public void noone() {
 
 		speed++;
 
-		if (move(rand.nextInt(2), rand.nextInt(2), rand.nextInt(2)) == false)
-		{
-			while (move(rand.nextInt(4), rand.nextInt(2), rand.nextInt(2)) == false)
-			{
+		if (move(rand.nextInt(2), rand.nextInt(2), rand.nextInt(2)) == false) {
+			while (move(rand.nextInt(4), rand.nextInt(2), rand.nextInt(2)) == false) {
 				move(rand.nextInt(2), rand.nextInt(2), rand.nextInt(2));
 				speed++;
 			}
@@ -158,14 +138,12 @@ public class SimpleEnemy extends Entity implements Damageable
 	}
 
 	@Override
-	public int getHealth()
-	{
+	public int getHealth() {
 		return health;
 	}
 
 	@Override
-	public void takeDamage(int amount)
-	{
+	public void takeDamage(int amount) {
 		health -= amount;
 		if (health < 0)
 			setActive(false);
