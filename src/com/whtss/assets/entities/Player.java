c8748dbab7c3e246b53dfc9a61d6861d7e0fa231
Cameron Jenkins <cameronjenkins@localhost>
2017-05-15 07:53:35 -0400

    their is a probblem someweir

--- src/com/whtss/assets/entities/Player.java
+++ src/com/whtss/assets/entities/Player.java
@@ -15,196 +15,175 @@ import com.whtss.assets.render.animations.CompoundAnimation;
 import com.whtss.assets.render.animations.Laser;
 import com.whtss.assets.render.animations.TileDamage;
 
-public class Player extends Entity implements Damageable
-{
-    int speed = 7;
+public class Player extends Entity implements Damageable {
+	int speed = 7;
 	int move = 0;
 	int health = 100;
-	
 
-	public Player(HexPoint location, Level level)
-	{
+	public Player(HexPoint location, Level level) {
 		super(location, level);
 	}
 
 	@UIEventHandle(value = "Next Turn", turn = "Player")
-	public void resetMoves() //throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException
+	public void resetMoves() // throws UnsupportedAudioFileException,
+								// IOException, LineUnavailableException,
+								// InterruptedException
 	{
-		//SoundStuff cam = new SoundStuff();
-		//cam.swnat();
+		// SoundStuff cam = new SoundStuff();
+		// cam.swnat();
 		move = 0;
 	}
 
-	public int gethealth()
-	{
+	public int gethealth() {
 		return health;
 	}
 
-	public void walk(int da, int db, int dhy)
-	{
-//		if(flametiles.size()>1){
-//		for(HexPoint e: flametiles){
-//		if (getLocation().mABY(da, db, 2 * dhy).equals(e))
-//		{
-//			takeDamage(10);
-//		}
-//		}
-//		}
+	public void walk(int da, int db, int dhy) {
+		// if(flametiles.size()>1){
+		// for(HexPoint e: flametiles){
+		// if (getLocation().mABY(da, db, 2 * dhy).equals(e))
+		// {
+		// takeDamage(10);
+		// }
+		// }
+		// }
 		int dist = Math.abs(da) + Math.abs(db) + Math.abs(dhy);
-		if (getLocation().mABY(da, db, 2 * dhy).equals(getLevel().getEnd()))
-		{
+		if (getLocation().mABY(da, db, 2 * dhy).equals(getLevel().getEnd())) {
 			setActive(false);
 			getLevel().getUIInterface().selectTile(null);
 			getLevel().addPersistantPlayer(this);
-		}
-		else if (dist + move <= speed)
-		{
-			if (move(da, db, dhy))
-			{
+		} else if (dist + move <= speed) {
+			if (move(da, db, dhy)) {
 				move += dist;
 				getLevel().getUIInterface().selectTile(getLocation());
 			}
 
-			if(move >= speed)
+			if (move >= speed)
 				getLevel().getUIInterface().selectTile(null);
-		} 
-		else
+		} else
 			getLevel().getUIInterface().selectTile(null);
 	}
 
-	@UIEventHandle(value = "Key_P", turn = "Player")
-	public void attack(Entity target) throws UnsupportedAudioFileException, IOException, LineUnavailableException
-	{	
-		SoundStuff cam = new SoundStuff();
-		cam.Phazing();
-		if(move + 2 > speed)
-		{
-			getLevel().getUIInterface().selectTile(null);
-			return;
-		}
-		if(target == null)
-			return;
-		if(!(target instanceof Damageable))
-			return;
-		if(!target.isActive())
-			return;
-		
-		final int d = getLocation().dist(target.getLocation());
-		if(d > 4)
-			return;
-		move += 2;
-		((Damageable)target).takeDamage(10 * (5 - d));
-		getLevel().getUIInterface().startAnimation(new CompoundAnimation.Sequential(
-				new Laser(getLocation(), target.getLocation()), 
-				new TileDamage(target.getLocation()))
-			);
-		if(move >= speed)
-			getLevel().getUIInterface().selectTile(null);
-		else
-			getLevel().getUIInterface().selectTile(getLocation());
-	}
+//	@UIEventHandle(value = "Key_P", turn = "Player")
+//	public void attack(Entity target) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+//		SoundStuff cam = new SoundStuff();
+//		cam.Phazing();
+//		if (move + 2 > speed) {
+//			getLevel().getUIInterface().selectTile(null);
+//			return;
+//		}
+//		if (target == null) {
+//			return;
+//		}
+//		if (!(target instanceof Damageable)) {
+//			return;
+//		}
+//		if (!target.isActive()) {
+//			return;
+//		}
+//		final int d = getLocation().dist(target.getLocation());
+//		if (d > 4){
+//			return;
+//	    	move += 2;
+//		}
+//		((Damageable) target).takeDamage(10 * (5 - d));
+//		getLevel().getUIInterface().startAnimation(new CompoundAnimation.Sequential(
+//				new Laser(getLocation(), target.getLocation()), new TileDamage(target.getLocation())));
+//		if (move >= speed)
+//			getLevel().getUIInterface().selectTile(null);
+//		else
+//			getLevel().getUIInterface().selectTile(getLocation());
+//	}
+
+	
+	
 	
 	@UIEventHandle(value = "Key_Q", turn = "Player")
-	public void walkNA(int modifiers, HexPoint target)
-	{
+	public void walkNA(int modifiers, HexPoint target) {
 		walk(-1, 0, 0);
 	}
 
 	@UIEventHandle(value = "Key_W", turn = "Player")
-	public void walkPY(int modifiers, HexPoint target)
-	{
+	public void walkPY(int modifiers, HexPoint target) {
 		walk(0, 0, 1);
 	}
 
 	@UIEventHandle(value = "Key_E", turn = "Player")
-	public void walkPB(int modifiers, HexPoint target)
-	{
+	public void walkPB(int modifiers, HexPoint target) {
 		walk(0, 1, 0);
 	}
 
 	@UIEventHandle(value = "Key_A", turn = "Player")
-	public void walkNB(int modifiers, HexPoint target)
-	{
+	public void walkNB(int modifiers, HexPoint target) {
 		walk(0, -1, 0);
 	}
 
 	@UIEventHandle(value = "Key_S", turn = "Player")
-	public void walkNY(int modifiers, HexPoint target)
-	{
+	public void walkNY(int modifiers, HexPoint target) {
 		walk(0, 0, -1);
 	}
 
-	@UIEventHandle(value = "Key_D", turn="Player")
-	public void walkPA(int modifiers, HexPoint target)
-	{
+	@UIEventHandle(value = "Key_D", turn = "Player")
+	public void walkPA(int modifiers, HexPoint target) {
 		walk(1, 0, 0);
 	}
+
 	@UIEventHandle(value = "Key_U", turn = "Player")
-	public void walkNAf(int modifiers, HexPoint target)
-	{
-		for(int f=0;f<=5;f++){
-		walk(-1, 0, 0);
+	public void walkNAf(int modifiers, HexPoint target) {
+		for (int f = 0; f <= 5; f++) {
+			walk(-1, 0, 0);
 		}
 	}
 
 	@UIEventHandle(value = "Key_I", turn = "Player")
-	public void walkPYf(int modifiers, HexPoint target)
-	{ 
-		for(int f=0;f<=5;f++){
-		walk(0, 0, 1);
+	public void walkPYf(int modifiers, HexPoint target) {
+		for (int f = 0; f <= 5; f++) {
+			walk(0, 0, 1);
 		}
 	}
 
 	@UIEventHandle(value = "Key_O", turn = "Player")
-	public void walkPBf(int modifiers, HexPoint target)
-	{
-		for(int f=0;f<=5;f++){
-		walk(0, 1, 0);
+	public void walkPBf(int modifiers, HexPoint target) {
+		for (int f = 0; f <= 5; f++) {
+			walk(0, 1, 0);
 		}
 	}
 
 	@UIEventHandle(value = "Key_J", turn = "Player")
-	public void walkNBf(int modifiers, HexPoint target)
-	{
-		for(int f=0;f<=5;f++){
-		walk(0, -1, 0);
+	public void walkNBf(int modifiers, HexPoint target) {
+		for (int f = 0; f <= 5; f++) {
+			walk(0, -1, 0);
 		}
 	}
 
 	@UIEventHandle(value = "Key_K", turn = "Player")
-	public void walkNYf(int modifiers, HexPoint target)
-	{
-		for(int f=0;f<=5;f++){
-		walk(0, 0, -1);
+	public void walkNYf(int modifiers, HexPoint target) {
+		for (int f = 0; f <= 5; f++) {
+			walk(0, 0, -1);
 		}
 	}
 
-	@UIEventHandle(value = "Key_L", turn="Player")
-	public void walkPAf(int modifiers, HexPoint target)
-	{
-		for(int f=0;f<=5;f++){
-		walk(1, 0, 0);
-	}
+	@UIEventHandle(value = "Key_L", turn = "Player")
+	public void walkPAf(int modifiers, HexPoint target) {
+		for (int f = 0; f <= 5; f++) {
+			walk(1, 0, 0);
+		}
 	}
 
 	@Override
-	public int getHealth()
-	{
+	public int getHealth() {
 		return health;
 	}
-	
-	public int getMP()
-	{
+
+	public int getMP() {
 		return speed;
 	}
 
 	@Override
-	public void takeDamage(int amount)
-	{
-//		System.out.println(getHealth());
+	public void takeDamage(int amount) {
+		// System.out.println(getHealth());
 		health -= amount;
-		if(getHealth() < 0)
-		{
+		if (getHealth() < 0) {
 			setActive(false);
 			getLevel().getUIInterface().startAnimation(new BigDamage());
 			getLevel().deadPlayer();
