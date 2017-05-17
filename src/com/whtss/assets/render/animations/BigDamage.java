8982c46a70f9634fc45bcef7a86937789d85b479
zacha <zacha@10.122.191.192>
2017-05-17 12:23:30 -0400

    Fixed invalid color bug

--- src/com/whtss/assets/render/animations/BigDamage.java
+++ src/com/whtss/assets/render/animations/BigDamage.java
@@ -14,7 +14,8 @@ public class BigDamage extends Animation
 	@Override
 	public void drawOverGUI(Graphics2D g, int s)
 	{
-		g.setColor(new Color(1f, 0f, 0f, 0.7f * (getLength() - T()) / getLength()));
+		int time = (int) Math.min(T(), getLength());
+		g.setColor(new Color(1f, 0f, 0f, 0.7f * (getLength() - time) / getLength()));
 		g.fill(g.getClip());
 	}
 }
